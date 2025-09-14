6fa36c207814da3e6ea5b34c679b64a9808dd558
Zeeshan Quamar <zshnqmr1@gmail.com>
2025-09-14 05:37:41 +0530

    ssh2-sftp-client.d.ts for types error

---
+++ src/types/ssh2-sftp-client.d.ts
@@ -0,0 +1,20 @@
+// src/types/ssh2-sftp-client.d.ts
+declare module 'ssh2-sftp-client' {
+  export interface SftpConnectOptions {
+    host: string;
+    port?: number;
+    username?: string;
+    password?: string;
+    privateKey?: string | Buffer;
+    passphrase?: string;
+    readyTimeout?: number;
+    hostVerifier?: (hash: string) => boolean;
+  }
+
+  export default class SftpClient {
+    connect(options: SftpConnectOptions): Promise<void>;
+    get(remotePath: string): Promise<Buffer | NodeJS.ReadableStream>;
+    end(): Promise<void>;
+  }
+}
+
